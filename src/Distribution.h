99ceabef41a4d1f8482dd26942ed3de5729c39bd
Macker <moojkoo@gmail.com>
2026-02-09 14:18:41 +0100

    init workspace structure

---
+++ src/Distribution.h
@@ -0,0 +1,39 @@
+#ifndef DISTRIBUTION_HEADER
+#define DISTRIBUTION_HEADER
+
+#include "Geometry.h"
+
+#include <vector>
+
+class Distribution {
+public:
+    virtual ~Distribution() = 0;
+    virtual Point sample() const = 0;
+    virtual double integrate_probability(const Polygon& region) const = 0;
+    virtual void add_point(const Point& p) = 0;
+};
+
+class NormalDistribution : public Distribution {
+    using covariance = std::vector<std::vector<double>>;
+    covariance cov;
+public:
+    NormalDistribution(const covariance& cov);
+    NormalDistribution(std::vector<Point> samples);
+    Point sample() const override;
+    double integrate_probability(const Polygon& region) const override;
+    void add_point(const Point& p) override;
+};
+
+class DiscreteDistribution : public Distribution {
+    std::vector<Point> points;
+    const size_t height_resolution, width_resolution;
+    std::vector<std::vector<double>> probability_grid;
+public:
+    DiscreteDistribution(size_t height_resolution, size_t width_resolution);
+    DiscreteDistribution(const std::vector<Point>& points, size_t height_resolution = 200, size_t width_resolution = 200);
+    Point sample() const override;
+    double integrate_probability(const Polygon& region) const override;
+    void add_point(const Point& p) override;
+};
+
+#endif
