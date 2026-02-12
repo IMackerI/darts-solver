5ce93d3d40d369430bf0029fbfdf4bb205c70001
Macker <moojkoo@gmail.com>
2026-02-12 15:15:12 +0100

    solver complete

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,1 +1,55 @@
 #include "Solver.h"
+#include "Random.h"
+
+#include <utility>
+#include <vector>
+#include <random>
+
+std::vector<Point> Solver::sample_aims() const {
+    std::vector<Point> aims;
+    auto [min_point, max_point] = game.get_target_bounds();
+
+    std::uniform_real_distribution<double> x_dist(min_point.x, max_point.x);
+    std::uniform_real_distribution<double> y_dist(min_point.y, max_point.y);
+
+    for (size_t i = 0; i < NumSamples; ++i) {
+        aims.emplace_back(x_dist(random_engine), y_dist(random_engine));
+    }
+
+    return aims;
+}
+
+Solver::Score Solver::expected_score(Game::State s, Point aim) {
+    auto states = game.throw_at(aim, s);
+    Solver::Score expected = 0;
+    double same_state_prob = 0;
+
+    for (const auto& [state, probability] : states) {
+        if (state == s) {
+            same_state_prob += probability;
+            continue;
+        }
+        
+        expected += solve(state) * probability;
+    }
+
+    if (same_state_prob >= 1.0 - EPSILON) return INFINITE_SCORE;
+    
+    expected /= (1.0 - same_state_prob);
+    return expected;
+}
+
+Solver::Score Solver::solve(Game::State s) {
+    if (memoization.contains(s)) { return memoization[s]; }
+
+    Solver::Score best_score = INFINITE_SCORE;
+
+    for (const auto& aim : sample_aims()) {
+        Solver::Score score = expected_score(s, aim);
+        if (score < best_score) {
+            best_score = score;
+        }
+    }
+    return memoization[s] = best_score;
+}
+
--- src/Solver.h
+++ src/Solver.h
@@ -12,10 +12,17 @@ class Solver {
 public:
     using Score = double;
 private:
+    static constexpr double EPSILON = 1e-9;
+    static constexpr double INFINITE_SCORE = 1e9;
+
+    const size_t NumSamples;
+
     std::unordered_map<Game::State, Score> memoization;
-    const Distribution& distribution;
+    const Game& game;
+
+    std::vector<Point> sample_aims() const;
 public:
-    Solver(const Distribution& distribution);
+    Solver(const Game& game, size_t num_samples = 10000) : NumSamples(num_samples), game(game) {};
     Score expected_score(Game::State s, Point aim);
     Score solve(Game::State s);
 };
